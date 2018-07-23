795410239687c2fcf5cbcad67f33f6f53cc965d8
oldman28 <rivaldo28497@gmail.com>
2018-07-23 23:26:43 +0400

    добавление, редактирование, удаление

--- CarProject/Controllers/HomeController.cs
+++ CarProject/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using CarProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,18 +18,111 @@ namespace CarProject.Controllers
             ViewBag.brand = Items;
             return View();
         }
-       // [HttpGet]
         public ActionResult Models(int id)
         {
             var item = db.Models.Where(p => p.BrandsId == id).ToList();
             return View(item);
         }
-        /*[HttpPost]
-        public ActionResult Models()
+        public ActionResult Parts(int id)
         {
-            var Items = db.Models;
-            ViewBag.model = Items;
+            var item = db.Parts.Where(p => p.ModelsId == id).ToList();
+            return View(item);
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = db.Parts.Find(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.ModelsId = new SelectList(db.Models, "Id", "ModelsName");
             return View();
-        }*/
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,PartsName,ModelsId,Price")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parts.Add(part);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ModelsId = new SelectList(db.Models, "Id", "ModelsName", part.ModelsId);
+            return View(part);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = db.Parts.Find(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ModelsId = new SelectList(db.Models, "Id", "ModelsName", part.ModelsId);
+            return View(part);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PartsName,ModelsId,Price")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(part).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ModelsId = new SelectList(db.Models, "Id", "ModelsName", part.ModelsId);
+            return View(part);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = db.Parts.Find(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Part part = db.Parts.Find(id);
+            db.Parts.Remove(part);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
